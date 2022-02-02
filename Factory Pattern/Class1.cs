eb9c5c4182b2641b931ff6bfee810656aa39233b
Joshua Chamberlain <jospcham@ut.utm.edu>
2022-02-01 20:33:44 -0600

    Add project files.

---
+++ Factory Pattern/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory_Pattern
+{
+    enum ShapeType
+    {
+        LINE,
+        CIRCLE,
+        RECTANGLE,
+        TRIANGLE
+    }
+}
---
+++ Factory Pattern/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+
+namespace Factory_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class ShapeFactory
+    {
+        GeometricShape getShape(ShapeType type)
+        {
+            
+            switch(type)
+            {
+                case == LINE:
+                    Console.WriteLine("L");
+                case == CIRCLE:
+                    Console.WriteLine("C");
+                case == RECTANGLE:
+                    Console.WriteLine("R");
+                case == TRIANGLE:
+                    Console.WriteLine("T");
+            }
+        }
+    }
+    abstract interface GeometricShape<ShapeType>
+    {
+           void draw()
+        {
+
+        }
+    }
+    class Line : GeometricShape<ShapeType>
+    {
+
+    }
+
+    class Circle : GeometricShape<ShapeType>
+    {
+
+    }
+
+    class Rectangle : GeometricShape<ShapeType>
+    {
+
+    }
+}
